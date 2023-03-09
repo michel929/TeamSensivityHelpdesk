4a1600df0c3d869fc6794a2cbca3e85c33f2d04a
Gruenig <michel.gruenig@gmail.com>
2023-03-09 15:14:33 +0100

    First Commit

---
+++ inc/head.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="de">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/slick/slick.css">
+    <link rel="stylesheet" href="assets/slick/slick-theme.css">
+    <!-- icon css-->
+    <link rel="stylesheet" href="assets/elagent-icon/style.css">
+    <link rel="stylesheet" href="assets/niceselectpicker/nice-select.css">
+    <link rel="stylesheet" href="assets/animation/animate.css">
+    <link rel="stylesheet" href="assets/mcustomscrollbar/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="css/style-main.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <title>Helpdesk | Team Sensivity</title>
+</head>
+<body data-scroll-animation="true">
+<div id="preloader">
+    <div id="ctn-preloader" class="ctn-preloader">
+        <div class="round_spinner">
+            <div class="spinner"></div>
+            <div class="text">
+                <img src="img/spinner_logo.png" alt="">
+                <h4><span>Team</span>Sensivity</h4>
+            </div>
+        </div>
+        <h2 class="head">Did You Know?</h2>
+        <p></p>
+    </div>
+</div>
